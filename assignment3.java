b2a52a21c322c67ca0ffc0da1110b830f247faa8
Nam Heon Kim <nhk.krhn2080773@gmail.com>
2023-10-31 05:30:35 +1030

    thread scaling

--- assignment3.java
+++ assignment3.java
@@ -4,6 +4,8 @@ import java.net.*;
 public class assignment3 {
 
     static int i = 0;
+    private static final Object lock = new Object();
+
     public static void main(String[] args) throws IOException {
         if (args.length < 1 || args.length > 4) {
             System.err.println("Usage: java assignment3 -l <listening port> -p <pattern>");
@@ -40,7 +42,9 @@ public class assignment3 {
                 Socket clientSocket = serverSocket.accept();
                 Thread thread = new Thread(new ClientHandler(clientSocket));
                 thread.start();
-                i++;
+                synchronized (lock) {
+                    i++;
+                }
             }
         } catch (IOException e) {
             System.err.println("Could not listen on port " + portNumber);
@@ -62,6 +66,7 @@ public class assignment3 {
 
     static class ClientHandler implements Runnable {
         private final Socket clientSocket;
+        private static final Object fileLock = new Object();
 
         ClientHandler(Socket socket) {
             this.clientSocket = socket;
@@ -73,16 +78,23 @@ public class assignment3 {
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
             ) {
-                FileWriter writer = new FileWriter("book_"+String.format("%02d",i)+".txt", true);
+                FileWriter writer = null;
+                //FileWriter writer = new FileWriter("book_"+String.format("%02d",i)+".txt", true);
                 String inputLine = in.readLine();
                 Node head = new Node(inputLine);
                 Node currentNode = head;
 
                 while (inputLine != null) {
                     inputLine = in.readLine();
-                    writer.write(currentNode.data+"\n");
-                    currentNode.book_Next = new Node(inputLine);
-                    currentNode = currentNode.book_Next;
+                    synchronized (fileLock) {
+                        writer = new FileWriter("book_" + String.format("%02d", i) + ".txt", true);
+                        writer.write(currentNode.data + "\n");
+                        writer.close();
+                    }
+                    synchronized (lock) {
+                        currentNode.book_Next = new Node(inputLine);
+                        currentNode = currentNode.book_Next;
+                    }
                 }
                 writer.close();
             } catch (IOException e) {
